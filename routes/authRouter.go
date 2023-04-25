e11a749e30775594338cc0c4d9c0f64fe2a7f2bc
Dzaky Mohammad <63341994+Dzaakk@users.noreply.github.com>
2023-04-25 21:58:17 +0700

    make db and middleware folder, some changes in routes

--- routes/authRouter.go
+++ routes/authRouter.go
@@ -1,1 +1,11 @@
 package routes
+
+import (
+	"github.com/Dzaakk/gin-jwt-mongodb/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func AuthRoutes(routes *gin.Engine) {
+	routes.POST("users/signup", controllers.Signup())
+	routes.POST("users/login", controllers.Login())
+}
--- routes/userRouter.go
+++ routes/userRouter.go
@@ -1,2 +1,14 @@
 package routes
 
+import (
+	"github.com/Dzaakk/gin-jwt-mongodb/controllers"
+	"github.com/Dzaakk/gin-jwt-mongodb/middleware"
+	"github.com/gin-gonic/gin"
+)
+
+func UserRoutes(routes *gin.Engine) {
+
+	routes.Use(middleware.Authenticate())
+	routes.GET("/users", controllers.GetUsers())
+	routes.GET("users/:user_id", controllers.GetUserById())
+}
