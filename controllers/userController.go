5fc92ccf6033a9dcc893ecddb2c218ad920ddca6
Dzaky Mohammad <63341994+Dzaakk@users.noreply.github.com>
2023-04-26 23:57:49 +0700

    add some code to getuserbyid

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,7 +1,11 @@
 package controllers
 
 import (
+	"net/http"
+
 	"github.com/Dzaakk/gin-jwt-mongodb/database"
+	"github.com/Dzaakk/gin-jwt-mongodb/helpers"
+	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -28,6 +32,15 @@ func GetUsers() {
 
 }
 
-func GetUserById() {
+func GetUserById() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		userId := c.Param("user_id")
 
+		if err := helpers.MatchUserTypeToUserId(c, userId); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+	}
 }
